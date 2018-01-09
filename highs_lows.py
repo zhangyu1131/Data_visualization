f9d658d65c2bb37643584e7de5c45ce577b194ec
Rain <923446353@qq.com>
2018-01-09 09:52:52 +0800

    1-9 add error-solve function

--- highs_lows.py
+++ highs_lows.py
@@ -2,18 +2,23 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename='sitka_weather_07-2014.csv'
+filename='death_valley_2014.csv'
 with open(filename) as f:
     reader=csv.reader(f)
     header_row=next(reader)
 
-    dates,highs=[],[]
+    dates,highs,lows=[],[],[]
     for row in reader:
-        current_date=datetime.strptime(row[0],"%Y-%m-%d")
-        dates.append(current_date)
-        high=int(row[1])
-        highs.append(high)
-
+        try:
+            current_date=datetime.strptime(row[0],"%Y-%m-%d")
+            high=int(row[1])
+            low=int(row[3])
+        except ValueError:
+            print(current_date,'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
     #查询列号
     # for index,column_header in enumerate(header_row):
     #     print(index,column_header)
@@ -21,8 +26,11 @@ with open(filename) as f:
 #可视化
 fig=plt.figure(figsize=(10,6))
 plt.plot(dates,highs,c='red')
+plt.plot(dates,lows,c='blue')
+#着色
+plt.fill_between(dates,lows,highs,facecolor='gray',alpha=0.6)#alpha表示透明度，0完全透明
 #设置图形格式
-plt.title("Daily high temperatures, July 2014",fontsize=24)
+plt.title("Daily high and low temperatures - 2014",fontsize=24)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()#绘制倾斜的日期标签
 plt.ylabel('Temperature(F)',fontsize=16)
